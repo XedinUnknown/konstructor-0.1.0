f40f74f89e680b119b3c8931cf19129fe6a4ca08
Anton Ukhanev <xedin.unknown@gmail.com>
2012-02-11 23:37:58 +0100

    The initial commit

---
+++ Reusing_Abstract.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @version 0.1.0
+ * @author Xedin Unknown <xedin.unknown+xdn@gmail.com>
+ */
+
+/**
+ * Description of ClassA
+ *
+ * @author Xedin Unknown <xedin.unknown+xdn@gmail.com>
+ */
+class ClassA {
+    
+    protected $_options;
+    
+    public function __construct($options) {
+        $this->_options = $options;
+        $this->_construct();
+    }
+    
+    protected function _construct() {
+        
+    }
+    
+    public function init() {
+        
+    }
+    
+    protected function _init() {
+        
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * @version 0.1.0
+ * @author Xedin Unknown <xedin.unknown+xdn@gmail.com>
+ */
+?>
